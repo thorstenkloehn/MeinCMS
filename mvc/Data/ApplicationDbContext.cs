1c2730a5206f01aa316ff399ea88f7db1748b72a
Thorsten Klöhn <thorstenkloehn@gmailcom>
2026-02-08 14:47:50 +0100

    Refactor code structure for improved readability and maintainability

---
+++ mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvc.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
