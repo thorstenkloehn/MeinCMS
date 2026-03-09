7cc2e683d3dc055ac02e7ada958476b240cf834d
Thorsten Klöhn <thorstnkloehn@gmail.com>
2026-03-09 04:33:34 +0100

    style: Format MarkdownInhalt property for consistency

--- mvc/Models/WikiArtikelVersion.cs
+++ mvc/Models/WikiArtikelVersion.cs
@@ -8,7 +8,7 @@ public class WikiArtikelVersion
 
 [Key]
 public long VersionNummer{get;set;}
-     public string MarkdownInhalt { get; set; }
+     public string MarkdownInhalt { get; set; } 
     public string HtmlInhalt { get; set; }
     public DateTime Zeitpunkt { get; set; }
     public List<string> Kategorie { get; set; } = [];
